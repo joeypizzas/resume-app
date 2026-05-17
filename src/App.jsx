d48d4160a2222b83095984b757aca546bf20b36b
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-17 10:18:38 -0400

    Invoked Footer in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 import { useState } from "react";
 import { Header } from "./components/Header/Header.jsx";
 import { ResumeContainer } from "./components/ResumeContainer/ResumeContainer.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
       <ResumeContainer></ResumeContainer>
+      <Footer></Footer>
     </div>
   );
 }
