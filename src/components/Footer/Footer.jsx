2820d81e15fa310ea34f544b710a8a4c53d875e8
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-17 10:18:18 -0400

    Added Footer component + style

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+// Footer component
+
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-text">
+        Created by{" "}
+        <a
+          href="https://github.com/joeypizzas"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          joeypizzas
+        </a>
+      </div>
+    </div>
+  );
+}
