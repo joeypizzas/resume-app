342651abd9a8afe2202d6296dbdfa672efbf3204
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-17 11:05:57 -0400

    Invoked ResumeBuilderForm and refactored background color/padding

--- src/components/ResumeBuilder/ResumeBuilder.jsx
+++ src/components/ResumeBuilder/ResumeBuilder.jsx
@@ -1,7 +1,12 @@
 // ResumeBuilder component
 
 import "./ResumeBuilder.css";
+import { ResumeBuilderForm } from "../ResumeBuilderForm/ResumeBuilderForm.jsx";
 
 export function ResumeBuilder() {
-  return <div className="resume-builder"></div>;
+  return (
+    <div className="resume-builder">
+      <ResumeBuilderForm header="Personal details"></ResumeBuilderForm>
+    </div>
+  );
 }
