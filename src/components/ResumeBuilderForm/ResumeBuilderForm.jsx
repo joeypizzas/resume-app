77a5be163c3fd1caabdcbb4531275c3e81dd16ef
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-17 11:05:14 -0400

    Started ResumeBuilderForm component and style

---
+++ src/components/ResumeBuilderForm/ResumeBuilderForm.jsx
@@ -0,0 +1,11 @@
+// ResumeBuilderForm component
+
+import "./ResumeBuilderForm.css";
+
+export function ResumeBuilderForm({ header }) {
+  return (
+    <div className="resume-builder-form">
+      <div className="resume-builder-form-header">{header}</div>
+    </div>
+  );
+}
