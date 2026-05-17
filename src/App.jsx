c9e158f16a8224b5b2eeb17cee3279bdd3fce0c8
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-17 10:04:59 -0400

    Invoked ResumeContainer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@
 import "./App.css";
 import { useState } from "react";
 import { Header } from "./components/Header/Header.jsx";
+import { ResumeContainer } from "./components/ResumeContainer/ResumeContainer.jsx";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <ResumeContainer></ResumeContainer>
     </div>
   );
 }
