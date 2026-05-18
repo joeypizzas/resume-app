ec363f1952af3ff1c0540e5b27f23ac993ed55ea
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-18 07:58:54 -0400

    Invoked FormField components + refactored form style

--- src/components/ResumeBuilderForm/ResumeBuilderForm.jsx
+++ src/components/ResumeBuilderForm/ResumeBuilderForm.jsx
@@ -1,11 +1,19 @@
 // ResumeBuilderForm component
 
 import "./ResumeBuilderForm.css";
+import { FormField } from "../FormField/FormField.jsx";
 
 export function ResumeBuilderForm({ header }) {
   return (
     <div className="resume-builder-form">
       <div className="resume-builder-form-header">{header}</div>
+      <FormField fieldHeader="Name" initialInput="Joey Pizzas"></FormField>
+      <FormField fieldHeader="Email" initialInput="joey@pizzas.com"></FormField>
+      <FormField
+        fieldHeader="Phone number"
+        initialInput="1234567890"
+      ></FormField>
+      <FormField fieldHeader="Location" initialInput="New York, NY"></FormField>
     </div>
   );
 }
