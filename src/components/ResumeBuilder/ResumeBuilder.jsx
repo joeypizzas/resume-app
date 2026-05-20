3c037038085ead969022ba4f7d68554f9f6172c1
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-20 07:43:11 -0400

    Added FormField children components to personal details form

--- src/components/ResumeBuilder/ResumeBuilder.jsx
+++ src/components/ResumeBuilder/ResumeBuilder.jsx
@@ -2,11 +2,26 @@
 
 import "./ResumeBuilder.css";
 import { ResumeBuilderForm } from "../ResumeBuilderForm/ResumeBuilderForm.jsx";
+import { FormField } from "../FormField/FormField.jsx";
 
 export function ResumeBuilder() {
   return (
     <div className="resume-builder">
-      <ResumeBuilderForm header="Personal details"></ResumeBuilderForm>
+      <ResumeBuilderForm header="Personal details">
+        <FormField fieldHeader="Name" initialInput="Joey Pizzas"></FormField>
+        <FormField
+          fieldHeader="Email"
+          initialInput="joey@pizzas.com"
+        ></FormField>
+        <FormField
+          fieldHeader="Phone number"
+          initialInput="1234567890"
+        ></FormField>
+        <FormField
+          fieldHeader="Location"
+          initialInput="New York, NY"
+        ></FormField>
+      </ResumeBuilderForm>
     </div>
   );
 }
