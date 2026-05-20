9556321cda7dd86885a9a1cf922a9347e1b29d76
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-20 07:51:11 -0400

    Added + styled FormFieldsContainer component

---
+++ src/components/FormFieldsContainer/FormFieldsContainer.jsx
@@ -0,0 +1,7 @@
+// FormFieldsContainer component
+
+import "./FormFieldsContainer.css";
+
+export function FormFieldsContainer({ children }) {
+  return <div className="form-fields-container">{children}</div>;
+}
