2179ae022dd179ea182146e0f8269b62e4071194
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-20 07:43:41 -0400

    Refactored component to pass generic children prop

--- src/components/ResumeBuilderForm/ResumeBuilderForm.jsx
+++ src/components/ResumeBuilderForm/ResumeBuilderForm.jsx
@@ -1,19 +1,12 @@
 // ResumeBuilderForm component
 
 import "./ResumeBuilderForm.css";
-import { FormField } from "../FormField/FormField.jsx";
 
-export function ResumeBuilderForm({ header }) {
+export function ResumeBuilderForm({ header, children }) {
   return (
     <div className="resume-builder-form">
       <div className="resume-builder-form-header">{header}</div>
-      <FormField fieldHeader="Name" initialInput="Joey Pizzas"></FormField>
-      <FormField fieldHeader="Email" initialInput="joey@pizzas.com"></FormField>
-      <FormField
-        fieldHeader="Phone number"
-        initialInput="1234567890"
-      ></FormField>
-      <FormField fieldHeader="Location" initialInput="New York, NY"></FormField>
+      {children}
     </div>
   );
 }
