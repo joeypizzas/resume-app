be31928ceb0770f843e31a6f8285beaed2c18397
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-21 07:27:37 -0400

    Added + styled date picker

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,1 +1,19 @@
 // DatePicker component
+
+import "./DatePicker.css";
+import MonthYearPicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export function DatePickerField({ fieldHeader, initialDate }) {
+  return (
+    <div className="field-container">
+      <div className="field-header">{fieldHeader}</div>
+      <MonthYearPicker
+        className="date-picker-field"
+        selected={initialDate}
+        dateFormat="MM/yyyy"
+        showMonthYearPicker
+      ></MonthYearPicker>
+    </div>
+  );
+}
