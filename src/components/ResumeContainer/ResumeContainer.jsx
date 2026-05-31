2114231bddf21bc7fed98e8a81b8eb2a0a9b4afb
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-31 09:21:00 -0400

    Invoked ResumeContainer component

--- src/components/ResumeContainer/ResumeContainer.jsx
+++ src/components/ResumeContainer/ResumeContainer.jsx
@@ -2,11 +2,13 @@
 
 import "./ResumeContainer.css";
 import { ResumeBuilder } from "../ResumeBuilder/ResumeBuilder.jsx";
+import { ResumePreview } from "../ResumePreview/ResumePreview.jsx";
 
 export function ResumeContainer() {
   return (
     <div className="resume-container">
       <ResumeBuilder></ResumeBuilder>
+      <ResumePreview></ResumePreview>
     </div>
   );
 }
