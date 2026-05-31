efbf81c72cae4caecd4cffb019b58f2099a1f34e
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-31 10:02:17 -0400

    Added/styled ResumePreviewHeader component

---
+++ src/components/ResumePreviewContainer/ResumePreviewContainer.jsx
@@ -0,0 +1,12 @@
+// ResumePreviewContainer component
+
+import "./ResumePreviewContainer.css";
+import { ResumePreview } from "../ResumePreview/ResumePreview.jsx";
+
+export function ResumePreviewContainer() {
+  return (
+    <div className="resume-preview-container">
+      <ResumePreview></ResumePreview>
+    </div>
+  );
+}
