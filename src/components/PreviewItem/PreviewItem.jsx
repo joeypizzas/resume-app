d13f4524b2b7f98f6337c57a3bff1522c7235338
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-31 11:28:47 -0400

    Added/started styling PreviewItem

---
+++ src/components/PreviewItem/PreviewItem.jsx
@@ -0,0 +1,28 @@
+// PreviewItem component
+
+import "./PreviewItem.css";
+
+export function PreviewItem({
+  organization,
+  location,
+  startDate,
+  endDate,
+  positionOrStudy,
+  description,
+}) {
+  return (
+    <div className="preview-item">
+      <div className="organization-location-date">
+        <div className="organization-location">
+          <div className="organization">{organization}</div>
+          <div className="location">{location}</div>
+        </div>
+        <div className="date">
+          {startDate} - {endDate}
+        </div>
+      </div>
+      <div className="position-or-study">{positionOrStudy}</div>
+      <div className="description">{description}</div>
+    </div>
+  );
+}
