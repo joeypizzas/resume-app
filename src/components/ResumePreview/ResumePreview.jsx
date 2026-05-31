29293fd58fcc72fd6839bfb023f9e3f1c7fa9b9b
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-05-31 10:04:21 -0400

    Invoked PreviewHeader

--- src/components/ResumePreview/ResumePreview.jsx
+++ src/components/ResumePreview/ResumePreview.jsx
@@ -1,7 +1,17 @@
 // ResumePreview component
 
 import "./ResumePreview.css";
+import { PreviewHeader } from "../PreviewHeader/PreviewHeader.jsx";
 
 export function ResumePreview() {
-  return <div className="resume-preview"></div>;
+  return (
+    <div className="resume-preview">
+      <PreviewHeader
+        location="New York, NY"
+        name="Joey Pizzas"
+        phone="1234567890"
+        email="joey@pizzas.com"
+      ></PreviewHeader>
+    </div>
+  );
 }
