caf9ffd2216390cd69d8fd8ef09747be4b2669e1
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-06-04 07:42:30 -0400

    Add experience + skills array to state

--- src/components/ResumeContainer/ResumeContainer.jsx
+++ src/components/ResumeContainer/ResumeContainer.jsx
@@ -1,11 +1,12 @@
 // ResumeContainer component
 
 import "./ResumeContainer.css";
+import { useState } from "react";
 import { ResumeBuilder } from "../ResumeBuilder/ResumeBuilder.jsx";
 import { ResumePreviewContainer } from "../ResumePreviewContainer/ResumePreviewContainer.jsx";
 
 export function ResumeContainer() {
-  /* const [resumeData, setResumeData] = useState({
+  const [resumeData, setResumeData] = useState({
     personal: {
       name: "Joey Pizzas",
       email: "joey@pizzas.com",
@@ -24,7 +25,24 @@ export function ResumeContainer() {
           "- Golden hands winner in tossin' pies competition (2019) - Pepperoni crown holder (2018) - University Honors (2018-2019)",
       },
     ],
-  }); */
+    experience: [
+      {
+        id: crypto.randomUUID(),
+        company: "Joey's Pizza Pies",
+        position: "Head pie guy",
+        startDate: "08/2019",
+        endDate: "04/2026",
+        location: "New York, NY",
+        description:
+          "Directly responsible for the creation of hundreds of delicious pizza pies each day.",
+      },
+    ],
+    skills: [
+      { id: crypto.randomUUID(), skill: "Kneading dough" },
+      { id: crypto.randomUUID(), skill: "Tossin' pies" },
+      { id: crypto.randomUUID(), skill: "Operating pizza oven" },
+    ],
+  });
 
   return (
     <div className="resume-container">
