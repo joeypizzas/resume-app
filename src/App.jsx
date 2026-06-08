67c448408b68f56db55fa90373fe2b5b23553cb2
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-06-08 08:01:21 -0400

    Deleted state management from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // App component
 
 import "./App.css";
-import { useState } from "react";
 import { Header } from "./components/Header/Header.jsx";
 import { ResumeContainer } from "./components/ResumeContainer/ResumeContainer.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
