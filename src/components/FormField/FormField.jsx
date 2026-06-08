2201ba87d54c9fe2ba28421de524a89fe025c4c1
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-06-08 08:01:33 -0400

    Updated to handle state

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -2,11 +2,11 @@
 
 import "./FormField.css";
 
-export function FormField({ fieldHeader, initialInput }) {
+export function FormField({ fieldHeader, value, onChange }) {
   return (
     <div className="field-container">
       <div className="field-header">{fieldHeader}</div>
-      <input className="form-field" defaultValue={initialInput}></input>
+      <input className="form-field" value={value} onChange={onChange}></input>
     </div>
   );
 }
