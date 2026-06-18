ce8e010dafd914265718ce38ee10bceacff06043
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-06-18 07:27:24 -0400

    Refactored header to add download button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,5 +3,11 @@
 import "./Header.css";
 
 export function Header() {
-  return <div className="header">Resume builder</div>;
+  return (
+    <div className="header">
+      <div className="header-spacer" />
+      <div className="header-title">Resume builder</div>
+      <button className="download-button">Download</button>
+    </div>
+  );
 }
