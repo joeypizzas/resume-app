457233b63919a0ae77f4fb3e852c0c23347d564d
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-06-18 19:58:42 -0400

    Added button props logic and fn

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import "./App.css";
 import { Header } from "./components/Header/Header.jsx";
 import { ResumeContainer } from "./components/ResumeContainer/ResumeContainer.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 
 function App() {
+  const previewRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    contentRef: previewRef,
+    documentTitle: "Resume-builder.pdf",
+  });
+
   return (
     <div className="app">
-      <Header></Header>
-      <ResumeContainer></ResumeContainer>
+      <Header onDownload={handlePrint}></Header>
+      <ResumeContainer previewRef={previewRef}></ResumeContainer>
       <Footer></Footer>
     </div>
   );
