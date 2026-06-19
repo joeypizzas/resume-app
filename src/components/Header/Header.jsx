86ebde5ca060fd43281d6ec587c21e71e4aa7c0e
Joe Kaufman <[joe.kaufman527@gmail.com]>
2026-06-18 20:06:04 -0400

    added download prompt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@
 
 import "./Header.css";
 
-export function Header() {
+export function Header({ onDownload }) {
   return (
     <div className="header">
       <div className="header-spacer" />
       <div className="header-title">Resume builder</div>
-      <button className="download-button">Download</button>
+      <button className="download-button" onClick={onDownload}>
+        Download
+      </button>
     </div>
   );
 }
